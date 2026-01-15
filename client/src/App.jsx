a9a1bb56d6b3b1586dd5b4cd8c3b352a2638f30f
Nayan <niranzanroy1@gmail.com>
2026-01-15 16:47:00 +0600

    router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,44 @@
-
-
-
+import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Home from './pages/homePage/Home';
+import Dashboard from './pages/dashboardPgae/Dashboard';
+import Unit from "./pages/unitPage/Unit"
+import UnitDetails from "./pages/unitDetailsPage/UnitDetails"
+import Note from "./pages/notePage/Note"
+import Ques from "./pages/quesPage/Ques"
+import Quiz from "./pages/quizPage/Quiz"
+import ExamQuiz from "./pages/examQuizPage/ExamQuiz"
+import Result from "./pages/resultPage/Result"
+import Login from "./pages/loginPage/Login"
+import SignUp from "./pages/signUpPage/SignUp"
+import ActiveAccount from "./pages/activeAccountPage/ActiveAccount"
+import NoPageFound from './pages/noPage/NoPageFound';
 
 function App() {
   
 
   return (
     <>
+    <Container>
       <p className="text-primary bg-black">hello</p>
-    
+      <Routes>
+         <Route path='/' element={<Home />}/>
+         <Route path='/dashboard' element={<Dashboard />}/>
+         <Route path='/dashboard/:subj' element={<Unit />}/>
+         <Route path='/dashboard/:subjName/:unitName' element={<UnitDetails />}/>
+         <Route path='/dashboard/:subjName/:unitName/note' element={<Note />}/>
+         <Route path='/dashboard/:subjName/:unitName/question' element={<Ques />}/>
+         <Route path='/dashboard/:subjName/:unitName/quiz' element={<Quiz />}/>
+         <Route path='/dashboard/:subjName/:unitName/exam' element={<ExamQuiz />}/>
+         <Route path='/result' element={<Result />}/>
+         <Route path='/login' element={<Login />}/>
+         <Route path='/register' element={<SignUp />}/>
+         <Route path='/active-account' element={<ActiveAccount />}/>
+         <Route path='*' element={<NoPageFound />}/>
+      </Routes>
+    </Container>
+      
+     
     </>
   )
 }
