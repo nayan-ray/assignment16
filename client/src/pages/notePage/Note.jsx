267b28849b373786d6234aa478370072fe0f7bed
Nayan <niranzanroy1@gmail.com>
2026-01-21 21:19:18 +0600

    v2

--- client/src/pages/notePage/Note.jsx
+++ client/src/pages/notePage/Note.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 import BreadCrumb from '../../components/breadCrumb/BreadCrumb'
+import "../../assets/global.css"
+import Header from '../../components/header/Header'
+import Footer from '../../components/footer/Footer'
+import "./note.css"
 
 const Note = () => {
   return (
-    <div>
-      <p>note page</p>
+    <div className="common-wrapper">
+      <Header />
       <BreadCrumb />
+      <div>
+        <h2 className='text-center mb-4'>Notes</h2>
+        <ul className="note-list">
+           <li className="note-item">
+            <div className='note-number'>1)</div>
+            <div className='note-body'>
+                <div className="body-top">What is the purpose of this note?</div> 
+                <div className="body-bottom">Explanation : I know the purpose of this note. Lorem ipsum .</div>
+            </div>
+           </li>
+            <li className="note-item">
+            <div className='note-number'>2)</div>
+            <div className='note-body'>
+                <div className="body-top">What is the purpose of this note?</div> 
+                <div className="body-bottom">Explanation : I know the purpose of this note. Lorem ipsum . Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat nam explicabo id iste voluptatem quae inventore, laborum earum eaque molestiae ab repudiandae voluptatibus commodi. Deleniti mollitia sunt optio laudantium libero?</div>
+            </div>
+           </li>
+        </ul>
+      </div>
+      <Footer />
     </div>
   )
 }
--- client/src/pages/quesPage/Ques.jsx
+++ client/src/pages/quesPage/Ques.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import "./ques.css"
+import "../../assets/global.css"
+import Header from '../../components/header/Header'
+import BreadCrumb from '../../components/breadCrumb/BreadCrumb'
+import Footer from '../../components/footer/Footer'
 
 const Ques = () => {
   return (
-    <div>Ques</div>
+    <div className="common-wrapper">
+         <Header />
+          <BreadCrumb />
+          <div>
+        <h2 className='text-center mb-4'>Model Question</h2>
+        <ul className="ques-list">
+           <li className="ques-item">
+               <div className="ques-body"></div>
+               
+           </li>
+        </ul>
+      </div>
+          <Footer />
+    </div>
   )
 }
 
