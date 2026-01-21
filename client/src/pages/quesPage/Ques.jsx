e37c410b1d31ad197782c990d536c14b7b2a832e
Nayan <niranzanroy1@gmail.com>
2026-01-21 22:31:42 +0600

    v3

--- client/src/pages/quesPage/Ques.jsx
+++ client/src/pages/quesPage/Ques.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./ques.css"
 import "../../assets/global.css"
 import Header from '../../components/header/Header'
@@ -6,17 +6,55 @@ import BreadCrumb from '../../components/breadCrumb/BreadCrumb'
 import Footer from '../../components/footer/Footer'
 
 const Ques = () => {
+  const [isAnswerVisible, setIsAnswerVisible] = useState(false);
   return (
     <div className="common-wrapper">
          <Header />
           <BreadCrumb />
           <div>
-        <h2 className='text-center mb-4'>Model Question</h2>
-        <ul className="ques-list">
-           <li className="ques-item">
-               <div className="ques-body"></div>
+            <h2 className='text-center mb-4'>Model Question</h2>
+            <ul className="list-container">
+               <li className="numbering-gap">
+                 <div className="number-text">1.</div>
+                 <div className="body-content">
+                    <p className="body-justify">This is the first model question. It contains a detailed explanation of the topic.</p>
+                    <p className='body-justify'>A) What is the purpose of this question</p>
+                     <p className='body-justify'>B) What is the purpose of this question</p>
+                      <p className='body-justify'>C) What is the purpose of this question</p>
+                      <div className='body-content'>
+                         <div className="answer-text text-center" onClick={() => setIsAnswerVisible(!isAnswerVisible)}>{isAnswerVisible ? "Hide Answer" : "Show Answer"}</div>
+                        {isAnswerVisible && (
+                           <div className="answer-body">
+                            <p className='body-justify'>Answer A : The purpose of this question is to test your understanding of the topic. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.</p>
+                            <p className='body-justify'>Answer B : The purpose of this question is to test your understanding of the topic. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.</p>
+                            <p className='body-justify'>Answer C : The purpose of this question is to test your understanding of the topic. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.</p>
+                         </div>
+                        )}
+                      </div>
+                 </div>
                
-           </li>
+              </li>
+
+               <li className="numbering-gap">
+                 <div className="number-text">1.</div>
+                 <div className="body-content">
+                    <p className="body-justify">This is the first model question. It contains a detailed explanation of the topic.</p>
+                    <p className='body-justify'>A) What is the purpose of this question</p>
+                     <p className='body-justify'>B) What is the purpose of this question</p>
+                      <p className='body-justify'>C) What is the purpose of this question</p>
+                      <div className='body-content'>
+                         <div className="answer-text text-center" onClick={() => setIsAnswerVisible(!isAnswerVisible)}>{isAnswerVisible ? "Hide Answer" : "Show Answer"}</div>
+                        {isAnswerVisible && (
+                           <div className="answer-body">
+                            <p className='body-justify'>Answer A : The purpose of this question is to test your understanding of the topic. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.</p>
+                            <p className='body-justify'>Answer B : The purpose of this question is to test your understanding of the topic. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.</p>
+                            <p className='body-justify'>Answer C : The purpose of this question is to test your understanding of the topic. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.</p>
+                         </div>
+                        )}
+                      </div>
+                 </div>
+               
+              </li>
         </ul>
       </div>
           <Footer />
