640d03ba6211573339b756dbe00140edd2601931
Nayan <niranzanroy1@gmail.com>
2026-01-31 10:01:33 +0600

    unit Details Completed

--- client/src/pages/unitDetailsPage/UnitDetails.jsx
+++ client/src/pages/unitDetailsPage/UnitDetails.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import Header from '../../components/header/Header'
+import Footer from '../../components/footer/Footer'
+import "./unitDetails.css"
+import BreadCrumb from '../../components/breadCrumb/BreadCrumb'
 
 const UnitDetails = () => {
-  return (
-    <div>UnitDetails</div>
+   return (
+    <div className='dashboard-container'>
+      <div className="dashboard-wrapper">
+          <Header/>
+          <BreadCrumb />
+          <div>
+            <h2 className='text-center mb-4'>Unit Details</h2>
+            <ul className='text-center list'>
+              <li className='py-2 item'>
+                 <div className="details-wrapper">
+                    <span className="unit-title">note</span>
+                    <span className='total-number'>20</span>
+                 </div>
+              </li>
+              
+              <li className='py-2 item'>
+                 <div className="details-wrapper">
+                    <span className="unit-title">note</span>
+                    <span className='total-number'>20</span>
+                 </div>
+              </li>
+
+              <li className='py-2 item'>
+                 <div className="details-wrapper">
+                    <span className="unit-title">note</span>
+                    <span className='total-number'>20</span>
+                 </div>
+              </li>
+
+            </ul>
+          </div>
+          <Footer />
+      </div>
+       
+    </div>
   )
 }
 
