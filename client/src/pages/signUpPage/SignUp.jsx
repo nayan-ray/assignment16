d7f0f3ddf3bb77902d0039656a54fe85f9736b96
Nayan <niranzanroy1@gmail.com>
2026-02-01 20:37:27 +0600

    sign up onechange handler

--- client/src/pages/signUpPage/SignUp.jsx
+++ client/src/pages/signUpPage/SignUp.jsx
@@ -17,41 +17,41 @@ const SignUp = () => {
                 <label htmlFor="user-name">
                    Student Name : 
                 </label>
-                <input type="text"  value={student?.name || ''} placeholder='Enter your name' required onChange={(e) => dispatch(setStudent({field: "name", value: e.target.value}))}/>
+                <input type="text" id='user-name' name='name'  value={student?.name || ''} placeholder='Enter your name' required onChange={(e) => dispatch(setStudent({field: e.target.name, value: e.target.value}))}/>
             </div>
             <div className="form-group">
                 <label htmlFor="user-email">
                    email : 
                 </label>
-                <input type="email" id='user-email' placeholder='Enter your email' required />
+                <input type="email" id='user-email' name='email' value={student?.email || ''} placeholder='Enter your email' required onChange={(e)=> dispatch(setStudent({field : e.target.name, value : e.target.value}))}/>
             </div>
             <div className="form-group">
                 <label htmlFor="user-password">
                    password : 
                 </label>
-                <input type="password" id='user-password' placeholder='Enter your password' required />
+                <input type="password" id='user-password' name='password' value={student?.password || ''} placeholder='Enter your password' required onChange={(e) => dispatch(setStudent({field: e.target.name, value: e.target.value}))}/>
             </div>
             <div className="form-group">
                 <label htmlFor="user-address">
                    address : 
                 </label>
-                <input type="text" id='user-address' placeholder='Enter your address' required />
+                <input type="text" id='user-address' name='address' value={student?.address || ''} placeholder='Enter your address' required onChange={(e) => dispatch(setStudent({field: e.target.name, value: e.target.value}))}/>
             </div>
             <div className="form-group">
                 <label htmlFor="user-phone">
                    phone : 
                 </label>
-                <input type="text" id='user-phone' placeholder='Enter your phone' required />
+                <input type="text" id='user-phone' name='phone' value={student?.phone || ''} placeholder='Enter your phone' required onChange={(e) => dispatch(setStudent({field: e.target.name, value: e.target.value}))}/>
             </div>
             <div className="form-group">
                 <label htmlFor="user-class">
                    class : 
                 </label>
                 {/* dropdown for class */}
-                <select className='dropdown-class' name="" id="user-class">
+                <select className='dropdown-class'  id="user-class" name='classId' value={student?.classId || ''} required onChange={(e) => dispatch(setStudent({field: e.target.name, value: e.target.value}))}>
                     <option value="">Select Class</option>
-                    <option value="class1">Ten</option>
-                    <option value="class2">Eight</option> 
+                    <option value="Ten">Ten</option>
+                    <option value="Eight">Eight</option> 
                     <option value="class3">Class 3</option>
                     <option value="class4">Class 4</option>
                     <option value="class5">Class 5</option>
